e4b571895ee4558e157376565e24e2163fa10471
melobix0-git <melobix0@gmail.com>
2026-08-16 22:31:36 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from './components/ui/toast'
+import { AuthProvider } from './hooks/useAuth'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+    <AuthProvider> 
        <App />
        <Toaster/>
+    </AuthProvider> 
     </BrowserRouter>
   </StrictMode>,
 )
